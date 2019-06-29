f98761ea9d4b1c7cac08a799d94bc797c2ccda8c
roelo34 <roeldeking@gmail.com>
2019-06-29 21:46:25 +0200

    Changed the querry to a more insightful one

--- show_db.py
+++ show_db.py
@@ -3,10 +3,11 @@ import MySQLdb
 def get_patient_history():
     db = MySQLdb.connect(host='127.0.0.1', user='root', database='patientinfo')
     cursor = db.cursor()
-    querry = """select patients.firstName, patients.lastName, behandelgeschiedenis.behandelID,
-    behandelingen.behandelDesc, behandelgeschiedenis.treatmentDate from patients inner join 
-    behandelgeschiedenis on patients.bsn = behandelgeschiedenis.patientID inner join behandelingen 
-    on behandelingen.behandelID = behandelgeschiedenis.behandelID order by treatmentDate desc;"""
+    querry = """select distinct patients.lastName, behandelingen.behandelDesc, behandelgeschiedenis.treatmentDate, 
+        tandartsen.dentistLastName from patients inner join behandelgeschiedenis on patients.patientID = 
+        behandelgeschiedenis.patientID inner join behandelingen on behandelingen.behandelID = 
+        behandelgeschiedenis.behandelID inner join tandartsen on tandartsen.dentistID = 
+        behandelgeschiedenis.dentistID order by treatmentDate desc;"""
     cursor.execute(querry)
     for entries in cursor:
         print(entries)
